ade007c269ba62cfcdf30006a6e8da4184396f7c
graycodedev6 <graycode.dev6@gmail.com>
2024-06-03 14:52:44 +0545

    next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,24 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
+
+const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  // disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  disable: process.env.NODE_ENV === 'development',  // Disable PWA in development mode
 });
 
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    return withPWA({
+      reactStrictMode: true,
+      // Any other next.js configurations can go here
+    });
+  }
+  return {
+    reactStrictMode: true,
+    // Any other next.js configurations can go here
+  };
 };
-
-module.exports = withPWA(nextConfig);
