09a49f1c7f23220ade4727978efc56b98ed731f2
graycodedev6 <graycode.dev6@gmail.com>
2024-06-05 12:48:27 +0545

    mainfest

--- next.config.js
+++ next.config.js
@@ -3,24 +3,17 @@ const {
   PHASE_PRODUCTION_BUILD,
 } = require("next/constants");
 
-const withPWA = require("next-pwa")({
-  dest: "public",
+const withPWA = require('next-pwa')({
+  dest: 'public',
   register: true,
   skipWaiting: true,
-  swDest:"public/sw.js",
-  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  sw: 'sw.js',
+  disable: process.env.NODE_ENV === 'development',
 });
 
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-    return withPWA({
-      reactStrictMode: true,
-      swcMinify: "public/sw.js",
-      crossOrigin: "anonymous",
-    });
-  }
-  return {
-    reactStrictMode: true,
-    crossOrigin: "anonymous",
-  };
-};
+module.exports = withPWA({
+  reactStrictMode: true,
+  swcMinify: true,
+  crossOrigin: 'anonymous',
+});
+
